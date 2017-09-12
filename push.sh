6218cba8c7d39befbe6840e1ee4b9b3bde9427f6
root <root@easatmidev.easa.net>
2017-09-12 16:52:02 -0300

    bash sync

--- push.sh
+++ push.sh
@@ -2,4 +2,5 @@ git config --global credential.helper cache
 git config credential.helper store
 git add -A
 git commit -m "bash sync"
-git push origin master
+# git push origin master
+git push origin aviso_centric
